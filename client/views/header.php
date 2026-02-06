e4dfbf4eb3af0c44f1a030739b90b60306266422
gnaht41 <gnaht41@gmail.com>
2026-02-06 21:27:28 +0700

    Trang chủ, đăng ký, đăng nhập

--- client/views/header.php
+++ client/views/header.php
@@ -7,6 +7,8 @@
     <title>Document</title>
     <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="style/trangchu.css">
+    <link rel="stylesheet" href="style/dangky.css">
+    <link rel="stylesheet" href="style/dangnhap.css">
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
